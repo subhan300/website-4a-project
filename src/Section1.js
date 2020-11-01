31c83a7815d6c19bc894d3957faa0cd50fd84d8b
subhan300 <64373402+subhan300@users.noreply.github.com>
2020-11-02 00:26:06 +0500

    Update Section1.js

--- src/Section1.js
+++ src/Section1.js
@@ -28,8 +28,8 @@ fontSize:"45px"}}>COPY WRITER
  
   <hr className="my-4" />
   <p style={{fontSize:"22px"}}>Enhance your communications with psychology-based copywriting and UX writing</p>
-  <a  className="btn  btn-sm disabled rounded-pill  bg-warning" tabindex="-1" 
-  role="button" >Send a message</a>  
+  <div className="btn  btn-sm disabled rounded-pill  bg-warning" tabindex="-1" 
+  role="button" >Send a message</div>  
  
 </div>
 
